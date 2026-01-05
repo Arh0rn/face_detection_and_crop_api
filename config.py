d45d9a1b0af26754e43d1bc257ee82f322bc1eed
Amir Kurmanbekov <amir.kurmanbekov@gmail.com>
2026-01-05 22:53:17 +0500

    remove unused config values

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # API settings
 API_TITLE: str = "Photo Processing API"
 API_DESCRIPTION: str = "API for processing and optimizing user photos."
-API_VERSION: str = "1.0.0"
+API_VERSION: str = "1.1.0"
 
 
 # Image processing settings
@@ -11,10 +11,6 @@ TARGET_WIDTH: int = 700
 TARGET_HEIGHT: int = 800
 PORTRAIT_PADDING_FACTOR: float = 2.2
 JPG_QUALITY: int = 90
-FACE_DETECTION_MIN_NEIGHBORS: int = 4
-FACE_DETECTION_SCALE_FACTOR: float = 1.1
-FACE_DETECTION_MIN_SIZE_W: int = 60
-FACE_DETECTION_MIN_SIZE_H: int = 60
 
 # Validation settings
 MAX_OUTPUT_SIZE_KB: int = 100
